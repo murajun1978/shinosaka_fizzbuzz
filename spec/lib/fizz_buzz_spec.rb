1d59ce2cd970cdc9138db4127e2b4230e3702d30
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2014-04-26 16:01:08 +0900

    refactor spec

--- spec/lib/fizz_buzz_spec.rb
+++ spec/lib/fizz_buzz_spec.rb
@@ -1,25 +1,31 @@
 require 'fizz_buzz'
 
 describe FizzBuzz do
+  let(:fizz_buzz){FizzBuzz.fizz_buzz(number: num)}
+  let(:num){1}
+
   it "引数に渡した数字が表示される" do
-    expect(FizzBuzz.fizz_buzz(number: 1)).to eq 1
+    expect(fizz_buzz).to eq 1
   end
 
   context "3で割り切れる場合" do
+    let(:num){3}
     it "Fizzが表示される" do
-      expect(FizzBuzz.fizz_buzz(number: 3)).to eq "Fizz"
+      expect(fizz_buzz).to eq "Fizz"
     end
   end
 
   context "5で割り切れる場合" do
+    let(:num){5}
     it "Buzzが表示される" do
-      expect(FizzBuzz.fizz_buzz(number: 5)).to eq "Buzz"
+      expect(fizz_buzz).to eq "Buzz"
     end
   end
 
   context "3でも5でも割り切れる場合" do
+    let(:num){15}
     it "FizzBuzzが表示される" do
-      expect(FizzBuzz.fizz_buzz(number: 15)).to eq "FizzBuzz"
+      expect(fizz_buzz).to eq "FizzBuzz"
     end
   end
 end
